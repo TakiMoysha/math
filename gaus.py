9d939857f62849403068d6350b52404a0ee35fc8
TakiMoysha <mpwema782@gmail.com>
2019-12-16 23:12:17 +0300

    update

--- gaus.py
+++ gaus.py
@@ -16,7 +16,7 @@ mean = 0 # среднее значение
 deviation = 0.5 # среднеквадратичное отклонение sigma
 k = 11 # Кол-во интервалов
 s = 0 # Поправочный множитель
-N = 1500
+N = 2000
 
 n = 6 # Обычно n берется 6 или 12
 
@@ -78,7 +78,7 @@ def getAxBar(data):
 xAx = np.arange(0, N, 0.1)
 yAx = []
 for i in range(len(xAx)): 
-    yAx.append(math.exp(-(xAx[i]-mean)**2/(2000000*deviation**2)))
+    yAx.append(math.exp(-(xAx[i]-mean)**2/(N**2*deviation**2)))
     # yAx.append((1/(deviation*math.sqrt(2*math.pi)))*math.exp(-(xAx[i]-mean)**2/(1000000*deviation**2)))
 ax_2.plot(xAx, yAx)
 
