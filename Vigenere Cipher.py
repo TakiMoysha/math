770560200ad4d62b4b350960d64e75c7b7fe1584
TakiMoysha <36836047+TakiMoysha@users.noreply.github.com>
2020-09-13 23:26:57 +0300

    Update Vigenere Cipher.py

--- Vigenere Cipher.py
+++ Vigenere Cipher.py
@@ -26,18 +26,58 @@ def encrypt(key, msg):
 
 
   steps = getSteps(key, len(msg))
+  print(steps)
   cryptMsg = list()
   
   for i, step in zip(msg, steps):
-    if ord(i) in range(1072, 1104):
+    if ord(i) in range(1072, 1104): # Проверка - Это русский алфавит
       cryptMsg.append(chr(ecryptIndexChar(i, 1072, 1104, step)))
-    elif ord(i) in range(97, 123):
+    elif ord(i) in range(97, 123): # Проверка - Это английский алфавит
       cryptMsg.append(chr(ecryptIndexChar(i, 97, 123, step)))
-    else:
+    else: # Если это не алфавит, то записываем символ в таком же виде, как он и был
       cryptMsg.append(i)
-  return ''.join(cryptMsg)
+  return ''.join(cryptMsg) # Это просто перевод списка в строку
+
 
 def decrypt(key, msg):
-  pass
+  def decryptIndexChar(char, startIndexChar, endIndexChar, step=0):
+    charDerypt = ord(char) - step
+    if charDerypt <= startIndexChar:
+      charDerypt = endIndexChar - (startIndexChar - charDerypt)
+    return charDerypt
 
-print(encrypt(key, message))
+
+  def getSteps(key, lenghtMsg):
+    def ecryptIndexChar(char, startIndexChar, endIndexChar, step=0):
+      charCrypt = ord(char) + step
+      if charCrypt >= endIndexChar: 
+        charCrypt = startIndexChar + (charCrypt - endIndexChar)
+      return charCrypt
+
+    key = (key*math.ceil(lenghtMsg/len(key)))[:lenghtMsg]
+    step = list()
+    for i in key:
+      if ord(i) in range(1072, 1104): # номера русского алфавита в кодировке
+        step.append(ecryptIndexChar(i, 1072, 1104)-1071)
+      elif ord(i) in range(97, 123): # номера английского алфавита в кодировке
+        step.append(ecryptIndexChar(i, 97, 123)-96)
+    return step
+  
+
+  steps = getSteps(key, len(msg))
+  print(steps)
+  decryptMsg = list()
+  
+  for i, step in zip(msg, steps):
+    if ord(i) in range(1072, 1104): # Проверка - Это русский алфавит
+      decryptMsg.append(chr(decryptIndexChar(i, 1072, 1104, step)))
+    elif ord(i) in range(97, 123): # Проверка - Это английский алфавит
+      decryptMsg.append(chr(decryptIndexChar(i, 97, 123, step)))
+    else: # Если это не алфавит, то записываем символ в таком же виде, как он и был
+      decryptMsg.append(i)
+  return ''.join(decryptMsg) # Это просто перевод списка в строку
+
+  
+crp = encrypt(key, message)
+print("Закодированно: ", crp)
+print("Раскодированно: ", decrypt(key, crp))
